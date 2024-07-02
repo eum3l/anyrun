d8032a4bdd25bd813698c0278afe3ecc6efc6f37
eum3l <77971322+eum3l@users.noreply.github.com>
2024-07-02 17:34:52 +0200

    feat(nix): add NixOS test

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     systems.url = "github:nix-systems/default-linux";
+    home-manager.url = "github:nix-community/home-manager";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -11,25 +12,26 @@
   };
 
   outputs =
-    {
-      self,
-      flake-parts,
-      systems,
-      ...
+    { self
+    , flake-parts
+    , systems
+    , home-manager
+    , ...
     }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ flake-parts.flakeModules.easyOverlay ];
       systems = import systems;
 
       perSystem =
-        {
-          self',
-          config,
-          pkgs,
-          ...
+        { self'
+        , config
+        , pkgs
+        , ...
         }:
         let
           inherit (pkgs) callPackage;
+          plugins = builtins.attrNames (builtins.readDir ./plugins);
+          cargoLock.lockFile = ./Cargo.lock;
         in
         rec {
           formatter = pkgs.nixfmt-rfc-style;
@@ -47,22 +49,25 @@
             ];
           };
 
-          checks = packages;
+          checks.default = callPackage ./nix/test.nix {
+            inherit packages plugins;
+            home-anyrun = self.homeManagerModules.default;
+            home-manager = home-manager.nixosModule;
+          };
+
           packages =
-            let
-              cargoLock.lockFile = ./Cargo.lock;
-              plugins = builtins.attrNames (builtins.readDir ./plugins);
-            in
             rec {
               default = anyrun;
               anyrun = callPackage ./nix/default.nix { inherit cargoLock; };
               anyrun-with-all-plugins = callPackage ./nix/default.nix { inherit cargoLock plugins; };
             }
             // builtins.listToAttrs (
-              builtins.map (name: {
-                inherit name;
-                value = callPackage ./nix/plugin.nix { inherit name cargoLock; };
-              }) plugins
+              builtins.map
+                (name: {
+                  inherit name;
+                  value = callPackage ./nix/plugin.nix { inherit name cargoLock; };
+                })
+                plugins
             );
 
           # Set up an overlay from packages exposed by this flake
@@ -71,7 +76,7 @@
 
       flake.homeManagerModules = rec {
         default = anyrun;
-        anyrun = import ./nix/hm-module.nix self;
+        anyrun = import ./nix/hm-module.nix self.packages;
       };
     };
 }
