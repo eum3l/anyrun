43be816f21d2da68a00c55dcd40cbee57b8ff6c1
eum3l <77971322+eum3l@users.noreply.github.com>
2024-07-02 17:32:30 +0200

    chore(hm-module): sync options
    + removals
      + x
      + y
      + hide_icons
      + close_on_click
    + additions
      + margin
      + edges
      + hide_plugin_info
      + hide_match_icons
      + steal_focus
      + save_entry_state
      + bottom_entry

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -1,14 +1,11 @@
-self:
-{
-  config,
-  pkgs,
-  lib,
-  ...
+packages:
+{ config
+, lib
+, pkgs
+, ...
 }:
 let
   cfg = config.programs.anyrun;
-
-  defaultPackage = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
   inherit (builtins)
     map
@@ -18,7 +15,7 @@ let
     stringLength
     ;
   inherit (lib.modules) mkIf mkMerge;
-  inherit (lib.options) mkOption mkEnableOption literalExpression;
+  inherit (lib.options) mkOption mkEnableOption;
   inherit (lib.lists) optional;
   inherit (lib.attrsets) mapAttrs' nameValuePair;
   inherit (lib.strings) toLower toUpper replaceStrings;
@@ -39,26 +36,24 @@ let
     ;
 in
 {
-  meta.maintainers = with lib.maintainers; [
-    n3oney
-    NotAShelf
-  ];
-
   options.programs.anyrun = {
     enable = mkEnableOption "anyrun";
+    package = lib.mkPackageOption packages.${pkgs.system} "anyrun" { };
 
-    package = mkOption {
-      type = nullOr package;
-      default = defaultPackage;
-      defaultText = literalExpression ''
-        anyrun.packages.''${pkgs.stdenv.hostPlatform.system}.default
-      '';
-      description = ''
-        Anyrun package to use. Defaults to the one provided by the flake.
-      '';
-    };
     config =
       let
+        numericOptionType = submodule {
+          options = {
+            absolute = mkOption {
+              type = nullOr int;
+              default = null;
+            };
+            fraction = mkOption {
+              type = nullOr float;
+              default = null;
+            };
+          };
+        };
         mkNumericOption =
           { default, description, ... }:
           mkOption {
@@ -68,18 +63,7 @@ in
               or
               { fraction = 0.4; };
             '';
-            type = submodule {
-              options = {
-                absolute = mkOption {
-                  type = nullOr int;
-                  default = null;
-                };
-                fraction = mkOption {
-                  type = nullOr float;
-                  default = null;
-                };
-              };
-            };
+            type = numericOptionType;
           };
 
         numericInfo = ''
@@ -97,21 +81,32 @@ in
           '';
         };
 
-        x = mkNumericOption {
-          default.fraction = 0.5;
+        margin = mkOption {
+          default = [ ];
+          example = [
+            {
+              absolute = 10;
+              fraction = 0.1;
+            }
+          ];
           description = ''
-            The horizontal position, adjusted so that { relative = 0.5; } always centers the runner.
+            The margin between the runner and the screen edges.
 
             ${numericInfo}
           '';
+          type = listOf numericOptionType;
         };
 
-        y = mkNumericOption {
-          default.fraction = 0.0;
+        edges = mkOption {
+          default = [ "Top" ];
+          type = listOf (enum [
+            "Top"
+            "Bottom"
+            "Left"
+            "Right"
+          ]);
           description = ''
-            The vertical position, works the same as x.
-
-            ${numericInfo}
+            Array of edges where to anchor window. Window will be stretched if two opposite edges specifyed.
           '';
         };
 
@@ -133,10 +128,40 @@ in
           '';
         };
 
-        hideIcons = mkOption {
+        stealFocus = mkOption {
           type = bool;
           default = false;
-          description = "Hide match and plugin info icons";
+          description = ''
+            If `true` will not let you operate with other windows while Anyrun opened
+          '';
+        };
+
+        saveEntryState = mkOption {
+          type = bool;
+          default = false;
+          description = ''
+            Save entred text on close and have it on open.
+          '';
+        };
+
+        bottomEntry = mkOption {
+          type = bool;
+          default = false;
+          description = ''
+            Place entry to the bottom.
+          '';
+        };
+
+        hideMatchIcons = mkOption {
+          type = bool;
+          default = false;
+          description = "Hide match icons";
+        };
+
+        hidePluginIcons = mkOption {
+          type = bool;
+          default = false;
+          description = "Hide plugin info icons";
         };
 
         ignoreExclusiveZones = mkOption {
@@ -225,21 +250,21 @@ in
         if cfg.config.plugins == null then
           [ ]
         else
-          map (
-            entry:
-            if lib.types.package.check entry then
-              "${entry}/lib/lib${replaceStrings [ "-" ] [ "_" ] entry.pname}.so"
-            else
-              entry
-          ) cfg.config.plugins;
+          map
+            (
+              entry:
+              if lib.types.package.check entry then
+                "${entry}/lib/lib${replaceStrings [ "-" ] [ "_" ] entry.pname}.so"
+              else
+                entry
+            )
+            cfg.config.plugins;
     in
     {
       assertions = [
         (assertNumeric cfg.config.width)
         (assertNumeric cfg.config.height)
-        (assertNumeric cfg.config.x)
-        (assertNumeric cfg.config.y)
-      ];
+      ] ++ (map (m: assertNumeric m) cfg.config.margin);
 
       warnings =
         if cfg.config.plugins == null then
@@ -260,16 +285,19 @@ in
         {
           "anyrun/config.ron".text = ''
             Config(
-              x: ${stringifyNumeric cfg.config.x},
-              y: ${stringifyNumeric cfg.config.y},
+              margin: [${lib.concatMapStringsSep "," (numeric: stringifyNumeric numeric) cfg.config.margin}],
+              edges: [${builtins.concatStringsSep "," cfg.config.edges}],
               width: ${stringifyNumeric cfg.config.width},
               height: ${stringifyNumeric cfg.config.height},
-              hide_icons: ${boolToString cfg.config.hideIcons},
+              hide_plugin_icons: ${boolToString cfg.config.hidePluginIcons},
+              hide_match_icons: ${boolToString cfg.config.hideMatchIcons},
               ignore_exclusive_zones: ${boolToString cfg.config.ignoreExclusiveZones},
               layer: ${capitalize cfg.config.layer},
               hide_plugin_info: ${boolToString cfg.config.hidePluginInfo},
-              close_on_click: ${boolToString cfg.config.closeOnClick},
               show_results_immediately: ${boolToString cfg.config.showResultsImmediately},
+              steal_focus: ${boolToString cfg.config.stealFocus},
+              save_entry_state: ${boolToString cfg.config.saveEntryState},
+              bottom_entry: ${boolToString cfg.config.bottomEntry},
               plugins: ${toJSON parsedPlugins},
             )
           '';
